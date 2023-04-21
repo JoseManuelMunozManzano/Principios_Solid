ff57a7cdda9869a736c956acf2d3617c9c24fe1e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-21 18:17:11 +0200

    Nombres de funciones, argumentos y parámetros

---
+++ clean-course-main/src/clean-code/03-functions.ts
@@ -0,0 +1,40 @@
+(() => {
+  // función para obtener información de una película por Id
+  function getMovieInfoById(id: string) {
+    console.log({ id });
+  }
+
+  // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+  function getMovieCatsById(id: string) {
+    console.log({ id });
+  }
+
+  // funcion para obtener el bio del actor por el id
+  function getBioActorById(id: string) {
+    console.log({ id });
+  }
+
+  // Crear una película
+  interface Movie {
+    cast: string[];
+    description: string;
+    rating: number;
+    title: string;
+  }
+  function createMovie({ title, description, rating, cast }: Movie) {
+    console.log({ title, description, rating, cast });
+  }
+
+  // Crea un nuevo actor
+  function checkFullName(fullName: string) {
+    console.log({ fullName });
+    return fullName === 'José M.';
+  }
+
+  function createActor(fullName: string, birthdate: Date): boolean {
+    if (checkFullName(fullName)) return false;
+
+    console.log('Crear actor', birthdate);
+    return true;
+  }
+})();
