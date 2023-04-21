70614c48d13b3303603df611951986a5fbede2d9
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-21 20:58:50 +0200

    Principio DRY - Aplicación

--- clean-course-main/src/clean-code/05-dry.ts
+++ clean-course-main/src/clean-code/05-dry.ts
@@ -3,17 +3,35 @@ type Size = '' | 'S' | 'M' | 'XL';
 class Product {
   constructor(public name: string = '', public price: number = 0, public size: Size = '') {}
 
+  isProductReady(): boolean {
+    // Usando DRY
+    // Servirá para todos los tipos de datos.
+    // El this hace referencia a la clase y las keys son las propiedades del constructor.
+    for (const key in this) {
+      //console.log(key, typeof this[key]);
+      switch (typeof this[key]) {
+        case 'string':
+          if ((this[key] as string).length <= 0) throw Error(`${key} is empty`);
+          break;
+        case 'number':
+          if ((this[key] as number) <= 0) throw Error(`${key} is zero`);
+          break;
+        default:
+          throw Error(`${typeof this[key]} is not supported`);
+      }
+    }
+
+    return true;
+  }
+
   toString() {
-    // Para cada nueva propiedad tendré que duplicar este código, que además ni siquiera debería estar aquí
-    if (this.name.length <= 0) throw Error('name is empty');
-    if (this.price <= 0) throw Error('price is zero');
-    if (this.size.length <= 0) throw Error('size is empty');
+    if (!this.isProductReady()) return;
 
     return `${this.name} (${this.price}), ${this.size}`;
   }
 }
 
 (() => {
-  const bluePants = new Product('Blue large pants', 10, 'M');
+  const bluePants = new Product('Blue pants', 10, 'S');
   console.log(bluePants.toString());
 })();
