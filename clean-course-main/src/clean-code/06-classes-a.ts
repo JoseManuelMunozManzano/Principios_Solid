a022baf417cb2e7b69ce2af305b3559d3a896c7b
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-22 07:06:56 +0200

    Clases en TS - Forma corta

--- clean-course-main/src/clean-code/06-classes-a.ts
+++ clean-course-main/src/clean-code/06-classes-a.ts
@@ -3,17 +3,9 @@
 
   type Gender = 'M' | 'F';
 
-  //Forma larga de crear clases
+  //Forma corta de crear clases
   class Person {
-    public name: string;
-    public gender: Gender;
-    public birthdate: Date;
-
-    constructor(name: string, gender: Gender, birthdate: Date) {
-      this.name = name;
-      this.gender = gender;
-      this.birthdate = birthdate;
-    }
+    constructor(public name: string, public gender: Gender, public birthdate: Date) {}
   }
 
   const newPerson = new Person('José M.', 'M', new Date('1978-30-10'));
