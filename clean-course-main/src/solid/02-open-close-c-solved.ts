2ac6230908a20fec1039253b7ec443cce51fbf4d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-04-23 06:33:12 +0200

    SOLID-Principio de abierto y cerrado (OPC)-Remover dependencia axios

--- clean-course-main/src/solid/02-open-close-c-solved.ts
+++ clean-course-main/src/solid/02-open-close-c-solved.ts
@@ -1,10 +1,19 @@
-// Desacoplamos axios
-import axios from 'axios';
+// NOTA: Esto es una clase adaptadora que se hace para no depender altamente de una dependencia de terceros, como
+//       sería axios. Hace más fácil las modificaciones.
+
+// Vamos a remover la dependencia de axios.
+// Pasos:
+//    - Hacer uninstall de axios: yarn remove axios
+//    - Eliminamos el import de axios.
+//    - Confirmar que solo falla esta clase. Lo demås debería seguir funcionando, puesto que lo desacoplamos antes.
+//    - Corregir la clase (se va a usar fetch).
+//    - Confirmar que funciona igual.
 
 export class HttpClient {
   async get(url: string) {
-    const { data, status } = await axios.get(url);
+    const resp = await fetch(url);
+    const data = await resp.json();
 
-    return { data, status };
+    return { data, status: resp.status };
   }
 }
